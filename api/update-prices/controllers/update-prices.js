f14196da917b23ba93112f4011ca884ee87e1b57
AirPC2 <i@memelu.ru>
2025-07-21 17:06:51 +0300

    Создание дополнительных полей с тремя ценами и создание эндпоинта по перезаписи этих полей

---
+++ api/update-prices/controllers/update-prices.js
@@ -0,0 +1,44 @@
+"use strict";
+
+module.exports = {
+  updatePrice: async (ctx) => {
+    const { id } = ctx.params;
+    const body = ctx.request.body;
+
+    try {
+      const program = await strapi.services.program.findOne({ id });
+
+      if (!program) {
+        return ctx.throw(404, "Программа не найдена");
+      }
+
+      if (!program.timenprice || program.timenprice.length === 0) {
+        return ctx.throw(400, "Отсутствует timenprice или он пустой");
+      }
+
+      if (
+        body.token !==
+        "zVHblsLnk7hZIVyn!XQehxtMaBu=0f0Wg6gngo?GfD==EQXNUaeR5v7efrwbYc4gfuHZp6q2g74NBmeznOih!YqvZV-vuZ=q5D--YXNDWr8N/5Pq9LwYglJgsT/LyV-k5d!W/eQ9V0Pf1C3twGygJef63egVUSu=cN?Ol5vF!iTqfwb/039CKMQJEtAk7jIQgR1C=97jQlbEfDMAiOCX4?hSmKXvXQ8r/XZ9vvTbkeCo9Czre=/ER2ORVozBigq3"
+      ) {
+        return ctx.throw(500, "Доступ запрещен");
+      }
+
+      const updatedProgram = await strapi.services.program.update(
+        { id },
+        {
+          PricePracticialTariffFromDB: body.pricePracticialTariff ?? "",
+          PriceBasicTariffFromDB: body.PriceBasicTariffFromDB ?? "",
+          PriceExpertTariffFromDB: body.PriceExpertTariffFromDB ?? "",
+          SaleToThisProductFromDB: body.SaleToThisProductFromDB ?? "",
+        }
+      );
+
+      return ctx.send({
+        message: id + " Обновление прошло успешно",
+      });
+
+    } catch (error) {
+      ctx.throw(500, error);
+    }
+  },
+};
